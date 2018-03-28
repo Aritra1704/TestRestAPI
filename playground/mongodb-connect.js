e40e94b0b3eef8825e36d58236de58deedb3e246
aritrapal <aritra.pal@zippr.in>
2018-03-28 19:29:48 +0530

    Initial Commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,49 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+// var obj = new ObjectID();
+// console.log(obj);
+// var user = {
+//     name: 'Aritra',
+//     age: 26
+// }
+
+// var{name} = user;
+// console.log(name);
+
+// console.loh()
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+    if(err) {
+        return console.log('Unable to connect to MongoDb server');
+    }
+
+    console.log('Connected to Mongo server');
+
+    const db = client.db('TodoApp');
+
+    // db.collection('Todos').insertOne({
+    //     text: 'Something to do',
+    //     completed: false
+    // }, (err, result) => {
+    //     if(err) {
+    //         return console.log('Unable to insert to do', err);
+    //     }
+
+    //     console.log(JSON.stringify(result.ops, undefined, 2));
+    // });
+
+//     db.collection('Users').insertOne({
+//         // _id: 123,
+//         name: 'Aritra',
+//         age: 28,
+//         location: 'Hyderabad' 
+//     }, (er, result) => {
+//         if(err)
+//             return console.log('Unable to insert user', err);
+
+//         // console.log(JSON.stringify(result.ops, undefined, 2));
+//         console.log(result.ops[0]._id.getTimestamp());
+//     });
+
+    client.close();
+});
---
+++ server/server.js
@@ -0,0 +1,94 @@
+const{ObjectId} = require('mongodb');
+
+var express = require('express');
+var bodyparser = require('body-parser');
+
+var {mongoose} = require('./db/mongoose.js');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+var app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyparser.json());
+
+app.post('/todos', (req, res) => {
+    var todo = new Todo({ 
+        text: req.body.text
+    });
+
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    })
+});
+
+app.post('/todos', (req, res) => {
+    var todo = new Todo({ 
+        text: req.body.text,
+        completed: req.body.completed
+    });
+
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    })
+});
+
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos});
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Started on port ${port}`);
+});
+
+module.exports = {app};
+
+// var testUser = new User({
+//     // email: 'aritra@test.com'
+// });
+
+// testUser.save().then((doc) => {
+//     console.log(JSON.stringify(doc, undefined, 2));
+// }, (e) => {
+//     console.log('Unable to save user', e);
+// });
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if(!ObjectId.isValid(id)) {
+        console.log('Id not valid');
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if(!todo) {
+            console.log('404');
+            return res.status(404).send();
+        }
+        console.log('Printing todo');
+        console.log(todo);
+        res.send({todo});
+    }).catch((e) => {
+        return res.status(400).send();
+    })
+    
+    Todo.findById(id).then((todo) => {
+        if(!todo) {
+            return res.status(404).send();
+        }
+
+        res.send({todo});
+    }).catch((e) => {
+        return res.status(400).send();
+    })
+   res.send(req.params);
+});
